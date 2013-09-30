8ed9b1463377437f1b130b18403bdc7200be939f
DepthDeluxe <cheinzmann3@gmail.com>
2013-09-30 13:49:37 -0400

    implemented a point class for pixel positions X and Y are integers.  I will probably add Vector2D which will contain floating point X and Y values (like in XNA)

---
+++ src/Point.cpp
@@ -0,0 +1,14 @@
+#include "Point.h"
+
+Point::Point(int x, int y) {
+	this->x = x;
+	this->y = y;
+}
+
+int Point::getX() {
+	return x;
+}
+
+int Point::getY() {
+	return y;
+}
---
+++ src/Point.h
@@ -0,0 +1,10 @@
+class Point {
+private:
+	int x, y;
+
+public:
+	Point(int x, int y);
+
+	int getX();
+	int getY();
+};
