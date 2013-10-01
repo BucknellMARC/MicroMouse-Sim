ee14e7d3f76f9ff194099836ea5dedd4fccdd360
DepthDeluxe <cheinzmann3@gmail.com>
2013-10-01 15:26:35 -0400

    include file with common constants

---
+++ src/define.h
@@ -0,0 +1,9 @@
+// contains all preprocessor definitions
+
+// screen dimensions
+#define SCREEN_WIDTH 500
+#define SCREEN_HEIGHT 500
+
+// maze dimensions
+#define MAZE_WIDTH 16
+#define MAZE_HEIGHT 16
