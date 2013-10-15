462dfb035c18c08d945bb1b9c839672be03ebce9
DepthDeluxe <cheinzmann3@gmail.com>
2013-10-15 13:54:41 -0400

    changed resolution to have even wall widths

--- src/define.h
+++ src/define.h
@@ -4,8 +4,8 @@
 #define DEFINE_H
 
 // screen dimensions
-#define SCREEN_WIDTH 500
-#define SCREEN_HEIGHT 500
+#define SCREEN_WIDTH 512
+#define SCREEN_HEIGHT 512
 
 // maze dimensions
 #define MAZE_WIDTH 16
