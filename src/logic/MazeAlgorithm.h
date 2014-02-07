f0eee6f6faffcdf5c2866818a799517437560491
DepthDeluxe <cheinzmann3@gmail.com>
2014-02-07 11:25:27 -0500

    put ff_map inside a struct, function changes ff_map should go in a struct so we can get the right sizeof without confusion.  There also may be some things (like the target) that we want to store away in that group.  We will assume that we will want to build the ff map at the center first.  We can then translate the target somewhere else since all of the relative positionings stay the same.

--- src/logic/MazeAlgorithm.h
+++ src/logic/MazeAlgorithm.h
@@ -5,10 +5,14 @@
 #include "MazeMap.h"
 #include "Robot.h"
 
-typedef int[MAZE_HEIGHT][MAZE_WIDTH] ff_map;
+typedef struct {
+	int array[MAZE_HEIGHT][MAZE_WIDTH];
+} ff_map;
+
+#define MALGO_FF_BAD -1
 
 // flood fill algorithms
-void malgo_floodfill_compute(int targetX, targetY, MazeMap *mm, ff_map *in);
+void malgo_floodfill_compute(MazeMap *mm, ff_map *in);
 void malgo_floodfill_recompute_target(int targetX, int targetY, ff_map *in);
 void malgo_floodfill_make_move(int xPos, int yPos, ff_map *map);
 
